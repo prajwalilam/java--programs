f0a5e65b74b7a803e4473b1d065fae7a8a8d11a3
Prajwal Chapagain <chapagainprajwal0@gmail.com>
2026-08-13 17:50:05 +0545

    Program using HashMap

---
+++ HashMapProgram.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+public class HashMapProgram {
+    public static void main(String[] args) {
+        HashMap<Integer, String> Record = new HashMap<Integer, String>();
+        Record.put(1,"Prajwal");
+       Record.put(2,"Lakshaya");
+        Record.put(3,"Kulbhakta");
+        System.out.println(Record);
+    }
+}
