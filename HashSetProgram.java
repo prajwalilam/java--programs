541e86d48930a96552c49fb68b063af4a2261ec3
Prajwal Chapagain <chapagainprajwal0@gmail.com>
2026-08-13 17:27:41 +0545

    Simple Java program using HashSet

---
+++ HashSetProgram.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+public class HashSetProgram {
+    public static void main(String[]args){
+        HashSet<String>District=new HashSet<String>();
+        District.add("Kathmandu");
+        District.add("Ilam");
+        District.add("Jhapa");
+        District.add("Palpa");
+        District.add("Kathmandu");
+        District.add("Ilam");
+        System.out.println(District);
+    }
+
+}
